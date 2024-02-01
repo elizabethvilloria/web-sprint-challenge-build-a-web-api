47a90c5eec192d39a4d05ec38c903e603741b238
elizabethvilloria <lizvilloria@gmail.com>
2024-02-01 01:27:47 -0500

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,6 +1,16 @@
 // Write your "actions" router here!
 const express = require('express');
-const Actions = require('./actions-model'); 
+const actions = require('./actions-model'); 
 const router = express.Router();
+
+router.get('/', (req, res) => {
+    actions.get()
+      .then(actions => {
+        res.json(actions);
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to get actions' });
+      });
+  });
 
 module.exports = router;
