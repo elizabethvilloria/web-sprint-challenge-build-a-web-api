b75fc5fb8b47b3d0947b96f18088eec102c312c4
elizabethvilloria <lizvilloria@gmail.com>
2024-02-01 01:22:49 -0500

    Wip

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,7 +1,33 @@
 // Write your "projects" router here!
 const express = require('express');
-const Projects = require('./projects-model');
+const projects = require('./projects-model');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    projects.get()
+      .then(projects => {
+        res.json(projects);
+        console.log('projects:', projects)
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to get projects' });
+      });
+  });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+  
+    projects.get(id)
+      .then(project => {
+        if (project) {
+          res.json(project);
+        } else {
+          res.status(404).json({ message: 'Project not found' });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({ message: 'Failed to get project' });
+      });
+  });
 
 module.exports = router;
